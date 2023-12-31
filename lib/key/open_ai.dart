e7929bd0530fc225118c9d6dd3f631a9328734d9
blackEye <69810869+xrwy@users.noreply.github.com>
2024-01-01 00:54:53 +0300

    Update open_ai.dart

--- lib/key/open_ai.dart
+++ lib/key/open_ai.dart
@@ -1,6 +1,6 @@
 class OpenAiApiKeyAndUrl {
   final String _apiURL = "https://api.openai.com/v1/chat/completions";
-  final String _apiKey = "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";
+  final String _apiKey = "OPEN_AI_API_KEY";
 
   String get apiUrl => _apiURL;
   String get apiKey => _apiKey;
