bcd4b0268084d70a0eb2f56989d9d2f2a40cd5c1
blackEye <69810869+xrwy@users.noreply.github.com>
2024-01-01 00:55:24 +0300

    Update voice_rss.dart

--- lib/key/voice_rss.dart
+++ lib/key/voice_rss.dart
@@ -1,5 +1,5 @@
 class VoiceRssApiKeyAndUrl {
-  final String _apiKey = "8c166a66b0a943b7826dd2cfc7fe052f";
+  final String _apiKey = "VOICE_API_KEY";
 
   getFullApiUrl(String apiKey, String language, String type, String text) {
     return "http://api.voicerss.org/?key=${apiKey.toString()}&hl=${language.toString()}&c=${type.toString()}&src=${text.toString()}";
